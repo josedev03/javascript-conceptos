127f12c9dac77bbc3c650e6296b7916038f94231
josedev03 <josedev03@users.noreply.github.com>
2020-03-30 19:30:56 -0500

    array elemento menor y mayor

--- utils.js
+++ utils.js
@@ -3,3 +3,9 @@ array = array.sort(function(a, b){return a -b})
 
 // eliminar elementos duplicados en el array
 unique = [...new Set(array)];
+
+// encontrar el elemento menor en un array
+const dato = Math.min(...array)
+
+encontrar el elemento mayor en un array
+const dato = Math.max(...array)
